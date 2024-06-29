1b05b37fb8d3f0798dbae7c5fce94d670c67d462
Shane <shanemonck11@hotmail.com>
2024-06-29 14:41:55 +0800

    ImageInput open a file

---
+++ docs/Writerside/topics/HelpfulLinks.md
@@ -0,0 +1,13 @@
+# Helpful
+
+
+https://github.com/KhronosGroup/3D-Formats-Guidelines/blob/main/KTXTools.md
+
+https://github.khronos.org/KTX-Software/
+
+https://github.com/KhronosGroup/KTX-Software/releases/tag/v4.3.2
+
+
+https://github.com/KhronosGroup/KTX-Software/blob/main/tools/ktx/command_create.cpp#L1790
+
+https://github.com/KhronosGroup/KTX-Software/blob/main/tools/ktx/command_create.cpp#L791
--- src/KTXCompressor/KTXCompressor/KTXCompressor.cpp
+++ src/KTXCompressor/KTXCompressor/KTXCompressor.cpp
@@ -3,10 +3,27 @@
 
 #include "KTXCompressor.h"
 
-using namespace std;
+#include <ktx.h>
+#include <OpenImageIO/imageio.h>
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+using namespace std;
+using namespace OIIO;
+
+int main() {
+
+    const std::string fileName = "textures/wood_diffuse_4096x4096.png";
+
+    auto imageInput = ImageInput::open(fileName);
+    if (!imageInput) {
+        throw std::runtime_error("Could not open file with name" + fileName);
+    }
+
+    const ImageSpec &imageSpec = imageInput->spec();
+
+    std::cout << "image width: " << imageSpec.width << "image height: " << imageSpec.height << std::endl;
+
+    imageInput->close();
+
+    return 0;
 }
+
